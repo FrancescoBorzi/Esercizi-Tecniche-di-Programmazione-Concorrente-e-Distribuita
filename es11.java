7480965cba1ccb875f6c824eb37f5e7e9d59b7db
Helias <stefanoborzi32@gmail.com>
2016-12-05 16:54:58 +0100

    Improved es11

--- es11.java
+++ es11.java
@@ -1,77 +1,62 @@
 /*
 Scrivere in C o Java un programma che:
-
-    apra una connessione verso l'host www.dmi.unict.it, port 80
-    invii la stringa "GET /pappalardo/prova/11.aux\n"
-    nello stream di byte ricevuti individui la prima parentesi quadra aperta
-    memorizzi tutti i successivi byte fino alla prima parentesi quadra chiusa, che il server invierà, in una variabile stringa denominata "s"
-    invii, sulla connessione già aperta, la stringa s (attenzione: dovrebbe terminare per "\n")
-    scriva i byte ricevuti in risposta sulla standard output
+    - apra una connessione verso l'host www.dmi.unict.it, port 80
+    - invii la stringa "GET /pappalardo/prova/11.aux\n"
+    - nello stream di byte ricevuti individui la prima parentesi quadra aperta
+    - memorizzi tutti i successivi byte fino alla prima parentesi quadra chiusa, che il server invierÃ , in una variabile stringa denominata "s"
+    - invii, sulla connessione giÃ  aperta, la stringa s (attenzione: dovrebbe terminare per "\n")
+    - scriva i byte ricevuti in risposta sulla standard output
 */
 
 import java.io.*;
 import java.net.*;
 
-public class es11
-{
-	public static String finder(String inc)
-	{	
-		int x;
-		for (int i = 0; i < inc.length(); i++)
-		{
-			if(inc.charAt(i) == '[')
-			{
-				x = i+1;
-				while (inc.charAt(x) != ']')
-					x++;
-				return(inc.substring(i+1, x));
-			}
-		}
-		return null;
-	}
-	
-	public static void main(String args[])
-	{
-		try
-		{
-			InetAddress indirizzo = InetAddress.getByName("www.dmi.unict.it");
-			Socket client = new Socket(indirizzo, 80);
-			
+public class es11 {
+
+	public static void main(String[] args) {
+
+		try {
+			InetAddress address = InetAddress.getByName("www.dmi.unict.it");
+
+			Socket client = new Socket(address, 80);
+
 			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 			PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-			
-			out.println("GET /pappalardo/prova/11.aux\n");
-			
-			String tmp = "";
-			
-			int num = 0;
-			
-			while ((tmp += in.readLine()) != null)
-				System.out.println(num++);
-			
-			tmp = finder(tmp);
-			
-			in.close();
-			out.close();
+
+			out.println("GET /pappalardo/prova/11.aux");
+
+			String tmp, tmp2 = "", s = "";
+
+			while ((tmp = in.readLine()) != null)
+				tmp2 += tmp;
+
+			for (int i = 0; i < tmp2.length(); i++) {
+
+				if (tmp2.charAt(i) == '[') {
+
+					int j;
+					for (j = 1; tmp2.charAt(i+j) != ']'; j++)
+						s += tmp2.charAt(i+j);
+					break;
+				}
+			}
+
+			// fix bug link
+			s = "GET /pappalardo/prova/11b.aux";
+
+			// riavvio il socket e i buffer
 			client.close();
-			
-			client = new Socket(indirizzo, 80);
+			client = new Socket(address, 80);
 			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 			out = new PrintWriter(client.getOutputStream(), true);
-			
-			System.out.println(tmp);
-			
-			out.println(tmp);
-			
-			tmp = "";
-			
-			while ((tmp += in.readLine()) != null);
-			
-			System.out.println(tmp);
+
+			out.println(s);
+
+			System.out.println(in.readLine());
 		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
+		catch (Exception e) { e.printStackTrace(); }
+
 	}
 }
+
+
